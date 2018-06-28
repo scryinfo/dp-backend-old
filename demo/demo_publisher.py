b6c7a8f5db246dd03f96f5f86ef17d73d400ae5a
chuckume <chuckchinaqc@gmail.com>
2018-06-28 15:57:32 +0800

    updated demo

--- demo/demo_publisher.py
+++ demo/demo_publisher.py
@@ -6,8 +6,8 @@ metadata_path='./metadata/'
 publisher_path='http://localhost:2222/'
 scry_path='http://localhost:1234/'
 userpayload={'username':'22','password':'22'}
-scry_path='https://dev.scry.info:443/scry2/'
-publisher_path='https://dev.scry.info:443/meta/'
+#scry_path='https://dev.scry.info:443/scry2/'
+#publisher_path='https://dev.scry.info:443/meta/'
 
 
 
@@ -23,7 +23,6 @@ def publisher(meta_payload,path=publisher_path,jwt_server_path=scry_path,user_pa
     headers = {"Authorization": "JWT "+jwtToken+'a'}
 
     r = requests.post('/publisher'+route,files=meta_payload,headers=headers)
-#    result=r.text
     return result
 
 def create_category(meta,path=publisher_path,jwt_server_path=scry_path,upayload=userpayload):
@@ -31,7 +30,7 @@ def create_category(meta,path=publisher_path,jwt_server_path=scry_path,upayload=
     headers = {"Authorization": "JWT "+jwtToken}
     r = requests.post(path+'categories', json = meta,headers=headers)
     result=r.text
-    print(result)
+#    print(result)
     return result
 
 def get_categories(path=publisher_path,jwt_server_path=scry_path,user_payload=userpayload):
@@ -44,18 +43,17 @@ def get_categories(path=publisher_path,jwt_server_path=scry_path,user_payload=us
 
 
 
-def publish_data(data_file,listing_file,publisher_path=publisher_path,scry_path=scry_path,userpayload=userpayload):
+def publish_data(data_file_path,listing_file_path,publisher_path=publisher_path,scry_path=scry_path,userpayload=userpayload):
     jwtToken=get_jwt_scry(scry_path,userpayload)
     headers = {"Authorization": "JWT "+jwtToken}
 
-    f1=open(data_file,'rb')
-    f2= open(listing_file)
+    f1=open(data_file_path,'rb')
+    f2= open(listing_file_path)
     files = {'data': f1,'listing_info':f2}
 
-    print (files)
     r = requests.post(publisher_path+'publisher',files=files,headers=headers)
-    result=r.text
-    print(result)
+
+    return r.text
 
 def publish_data_no_data(data_file,listing_file,publisher_path=publisher_path,scry_path=scry_path,userpayload=userpayload):
     jwtToken=get_jwt_scry(scry_path,userpayload)
@@ -67,7 +65,7 @@ def publish_data_no_data(data_file,listing_file,publisher_path=publisher_path,sc
 
     r = requests.post(publisher_path+'publisher',files=files,headers=headers)
     result=r.text
-    print(result)
+    return result
 
 def publish_data_no_listing(data_file,listing_file,publisher_path=publisher_path,scry_path=scry_path,userpayload=userpayload):
     jwtToken=get_jwt_scry(scry_path,userpayload)
@@ -79,7 +77,7 @@ def publish_data_no_listing(data_file,listing_file,publisher_path=publisher_path
 
     r = requests.post(publisher_path+'publisher',files=files,headers=headers)
     result=r.text
-    print(result)
+    return result
 
 def listing_category_id(cat_id,publisher_path=publisher_path,scry_path=scry_path,userpayload=userpayload):
     jwtToken=get_jwt_scry(scry_path,userpayload)
@@ -107,24 +105,29 @@ def listing_category_id(cat_id,publisher_path=publisher_path,scry_path=scry_path
 #publish_data_no_listing('/home/chuck/scry3/publisher/demo/data/schedule.csv','/home/chuck/scry3/publisher/demo/listing_info/Schedule_listing.json')
 
 
+
+# CREATE CATEGORIES
+
+meta_path='/home/chuck/scry3/publisher/demo/metadata/'
+meta_files=os.listdir(meta_path)
+for files in meta_files:
+    metadata=json.load(open(meta_path+files))
+    create_category(metadata)
+
+
 data_path='/home/chuck/scry3/publisher/demo/data/'
 listing_path='/home/chuck/scry3/publisher/demo/listing_info/'
 
 with open('./demo.json') as jsonfile:
-    test_data=json.load(jsonfile)
+    test_scenario=json.load(jsonfile)
 
-meta_path='/home/chuck/scry3/publisher/demo/metadata/'
-meta_files=os.listdir(meta_path)
-
-# CREATE CATEGORIES
-#for files in meta_files:
-#    metadata=json.load(open(meta_path+files))
-#    create_category(metadata)
-
-
-for i in test_data:
-    print (i['Listing'])
+for i in test_scenario:
+#    try:
+#        assert(publish_data(data_path+i['Data'],listing_path+i['Listing'])==i['TestResult'])
+#    except:
+    print(publish_data(data_path+i['Data'],listing_path+i['Listing'])==i['TestResult'])
     print(publish_data(data_path+i['Data'],listing_path+i['Listing']))
+
 
 #print(listing_category_id(51))
 #print(listing_category_id(217))
